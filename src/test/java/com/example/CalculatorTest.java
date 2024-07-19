1e5a1fc591b1c66df4525365ef97108f5701aa4b
asekawa <40081059+asekawa@users.noreply.github.com>
2024-07-20 00:22:26 +0530

    Update CalculatorTest.java

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -14,13 +14,13 @@ import junit.framework.TestCase;
 public class CalculatorTest extends TestCase {
 
     private final Calculator calculator = new Calculator();
-//    public void testAdd() {
-//        System.out.println("Running testAdd");
-//        assertEquals(5, calculator.add(2, 3));
-//    }
+   public void testAdd() {
+       System.out.println("Running testAdd");
+       assertEquals(5, calculator.add(2, 3));
+   }
 
-//    public void testSubtract() {
-//        System.out.println("Running testSubtract");
-//        assertEquals(1, calculator.subtract(3, 2));
-//    }
+   public void testSubtract() {
+       System.out.println("Running testSubtract");
+       assertEquals(1, calculator.subtract(3, 2));
+   }
 }
